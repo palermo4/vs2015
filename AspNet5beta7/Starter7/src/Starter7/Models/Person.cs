8c37fdc50bc3a352afd11b42eb9e7592c91eb4a4
J. Michael Palermo IV <j.michael@palermo4.com>
2015-09-04 00:11:15 -0700

    ASP.NET beta 7 added

---
+++ AspNet5beta7/Starter7/src/Starter7/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace Starter7.Models
+{
+    public class Person
+    {
+        private string _FirstName = "John";
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+        private string _LastName = "Doe";
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+        public string GetLastNameFirst()
+        {
+            return string.Format("{0}, {1}", LastName.ToUpper(), FirstName);
+        }
+        public string GetTestData(string testName)
+        {
+            var dashLength = 25 - testName.Length;
+            var dashes = string.Concat(Enumerable.Repeat("-", dashLength));
+            return string.Format("{{{0}}} {1} {2}", testName, dashes, FullName);
+        }
+    }
+}
---
+++ AspNet5beta7/Starter7/src/Starter7/Models/Person6.cs
@@ -0,0 +1,20 @@
+﻿using static System.String;
+using static System.Linq.Enumerable;
+
+namespace Starter7.Models
+{
+    public class Person6
+    {
+        // auto property initializer
+        public string FirstName { get; set; } = "John";
+        public string LastName { get; set; } = "Doe";
+        public string FullName => $"{FirstName} {LastName}";
+        public string GetLastNameFirst() => $"{LastName.ToUpper()}, {FirstName}";
+        public string GetTestData(string testName)
+        {
+            var dashLength = 25 - testName.Length;
+            var dashes = Concat(Repeat("-", dashLength));
+            return $"{{{testName}}} {dashes} {FullName}";
+        }
+    }
+}
